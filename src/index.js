5ee4139cbe860471997f36699399b3a4050c4fc6
tommydrozd <tomekdro@gmail.com>
2024-05-03 21:06:38 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,60 +1,66 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+import './styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css';
 
-const elements = {
-  selectEl: document.querySelector('.breed-select'),
-  textMarkEl: document.querySelector('.cat-info'),
-  loaderEl: document.querySelector('.loader'),
-  errorEl: document.querySelector('.error'),
+const ref = {
+    selector: document.querySelector('.breed-select'),
+    divCatInfo: document.querySelector('.cat-info'),
+    loader: document.querySelector('.loader'),
+    error: document.querySelector('.error'),
+};
+const { selector, divCatInfo, loader, error } = ref;
+
+loader.classList.replace('loader', 'is-hidden');
+error.classList.add('is-hidden');
+divCatInfo.classList.add('is-hidden');
+
+let arrBreedsId = [];
+fetchBreeds()
+.then(data => {
+    data.forEach(element => {
+        arrBreedsId.push({text: element.name, value: element.id});
+    });
+    new SlimSelect({
+        select: selector,
+        data: arrBreedsId
+    });
+    })
+.catch(onFetchError);
+
+selector.addEventListener('change', onSelectBreed);
+
+function onSelectBreed(event) {
+    loader.classList.replace('is-hidden', 'loader');
+    selector.classList.add('is-hidden');
+    divCatInfo.classList.add('is-hidden');
+
+    const breedId = event.currentTarget.value;
+    fetchCatByBreed(breedId)
+    .then(data => {
+        loader.classList.replace('loader', 'is-hidden');
+        selector.classList.remove('is-hidden');
+        const { url, breeds } = data[0];
+        
+        divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="350"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
+        divCatInfo.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
 };
 
-const { selectEl, textMarkEl, loaderEl, errorEl } = elements;
+function onFetchError(error) {
+    selector.classList.remove('is-hidden');
+    loader.classList.replace('loader', 'is-hidden');
 
-textMarkEl.classList.add('is-hidden');
+    Notify.failure('Oops! Something went wrong! Try reloading the page or select another cat breed!', {
+    });
+};
+   
 
-selectEl.addEventListener('change', createMarkUp);
 
-updateSelect();
 
-function updateSelect(data) {
-  fetchBreeds(data)
-    .then(data => {
-      loaderEl.classList.replace('loader', 'is-hidden');
 
-      let markSelect = data.map(({ name, id }) => {
-        return `<option value ='${id}'>${name}</option>`;
-      });
-      selectEl.insertAdjacentHTML('beforeend', markSelect);
-      new SlimSelect({
-        select: selectEl,
-      });
-    })
-    .catch(onFetchError);
-}
 
-function createMarkUp(event) {
-  loaderEl.classList.replace('is-hidden', 'loader');
-  selectEl.classList.add('is-hidden');
-  textMarkEl.classList.add('is-hidden');
-  const breedId = event.currentTarget.value;
-  fetchCatByBreed(breedId)
-    .then(data => {
-      loaderEl.classList.replace('loader', 'is-hidden');
-      selectEl.classList.remove('is-hidden');
-      const { url, breeds } = data[0];
 
-      textMarkEl.innerHTML = `<img src="${url}" alt="${breeds[0].name}" width="400"/><div class="box"><h2>${breeds[0].name}</h2><p>${breeds[0].description}</p><p><strong>Temperament:</strong> ${breeds[0].temperament}</p></div>`;
-      textMarkEl.classList.remove('is-hidden');
-    })
-    .catch(onFetchError);
-}
-
-function onFetchError() {
-  selectEl.classList.remove('is-hidden');
-  loaderEl.classList.replace('loader', 'is-hidden');
-  Notify.failure(
-    'Oops! Something went wrong! Try reloading the page or select another cat breed!'
-  );
-}
+    
